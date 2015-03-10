e1d7bd4f5ea28029008fc798e2994e3bddb71592
dmitriiz <dmitriiz@hotmail.com>
2015-03-10 20:42:07 +0200

    initial commit

---
+++ amp523/src/ru/dazzled/AMP523.java
@@ -0,0 +1,16 @@
+package ru.dazzled;
+
+/**
+ *
+ * @author dmitriiz
+ */
+public class AMP523 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
